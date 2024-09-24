bbc6d4baec0c7b542aced554ca6089f757c35f42
arthurtarg <163202564+arthurtarg@users.noreply.github.com>
2024-09-24 17:38:08 -0300

    Update loginteste.py

--- loginteste.py
+++ loginteste.py
@@ -1,29 +1,65 @@
 import flet as ft
 
 def main(page: ft.Page):
+    login_view(page)
+
+def login_view(page: ft.Page):
+    page.clean()
     page.horizontal_alignment = 'center'
     page.vertical_alignment = 'center'
     page.window_maximized = True
 
+    def Go_toRegister(e):
+        from registerteste import registerteste_view
+        registerteste_view(page)
+    
+    def login_click(e):
+        message_container.content = None
+        if not email.value or senha.value:
+            message_container.content= ft.Text("Preencha todos os campos.", color="red")
+            page.update()
+            return
+        #verificar_login(email.value, senha.value)
+        return
+    
+    message_container = ft.Container(alignment=ft.alignment.center)
+
+    email = ft.TextField(
+        label='Digite o seu email',
+        width=300,
+        height=40,
+        border_radius=40,
+        prefix_icon=ft.icons.EMAIL,
+        text_vertical_align=1,
+        keyboard_type=ft.KeyboardType.EMAIL,
+    )
+    senha = ft.TextField(
+        label='Digite a sua senha',
+        width=300,
+        height=40,
+        border_radius=40,
+        prefix_icon=ft.icons.LOCK,
+        text_vertical_align=1,
+        password=True,
+        can_reveal_password=True,
+        keyboard_type=ft.KeyboardType.VISIBLE_PASSWORD,
+    )
+
     Login = ft.Column([
         ft.Container(
-            width = page.window.width - 10,
-            height = page.window.height - 60,
-            border_radius = 10,
-            content = ft.Column([
+            width=800,
+            height=600,
+            border_radius=10,
+            content=ft.Column([
                 ft.Container(
-                    bgcolor = ft.colors.GREY_900,
-                    width = 400,
-                    height = 320,
-                    border_radius = 10,
-
+                    bgcolor=ft.colors.GREY_900,
+                    width=400,
+                    height=320,
+                    border_radius=10,
                     content=ft.Column([
-                        ft.Container( 
-                            padding = ft.padding.only(
-                                top=10,
-                                bottom = 12
-                            ),
-                            content = ft.Column([
+                        ft.Container(
+                            padding=ft.padding.only(top=10, bottom=12),
+                            content=ft.Column([
                                 ft.Text(
                                     value='Login',
                                     weight='bold',
@@ -31,49 +67,30 @@ def main(page: ft.Page):
                                 )
                             ])
                         ),
-
                         ft.Column([
-                            ft.TextField(
-                                hint_text='Digite o seu email',
-                                width=300,
-                                height=40,
-                                border_radius=40,
-                                prefix_icon=ft.icons.EMAIL,
-                                text_vertical_align=1,
-                                keyboard_type=ft.KeyboardType.EMAIL,
-                            ),
-                            ft.TextField(
-                                hint_text='Digite a sua senha',
-                                width=300,
-                                height=40,
-                                border_radius=40,
-                                prefix_icon=ft.icons.LOCK,
-                                text_vertical_align=1,
-                                password=True,
-                                can_reveal_password=True,
-                                keyboard_type=ft.KeyboardType.VISIBLE_PASSWORD,
-                            ),
+                            email,
+                            senha,
+                            message_container,
                             ft.ElevatedButton(
                                 text='Entrar',
-                                width=150,
-                                height=40
+                                width=300,
+                                height=40,
+                                on_click=login_click
                             ),
                             ft.Row([
-                                ft.TextButton(text='Criar Conta'),
+                                ft.TextButton(
+                                    text='Criar Conta',
+                                    on_click=Go_toRegister
+                                ),
                                 ft.TextButton('Esqueci minha senha')
                             ], width=300, alignment=ft.MainAxisAlignment.SPACE_BETWEEN)
-                        ],spacing=12),
-
-                        ft.Row([
-                            ft.IconButton(icon=ft.icons.EMAIL),
-                            ft.IconButton(icon=ft.icons.FACEBOOK),
-                            ft.IconButton(icon=ft.icons.TELEGRAM)
-                        ], alignment='center')
+                        ], spacing=12, horizontal_alignment='center'),
                     ], horizontal_alignment='center')
                 )
-            ], horizontal_alignment= 'center', alignment='center')
+            ], horizontal_alignment='center', alignment='center')
         )
     ])
     page.add(Login)
+    page.update()
 
 ft.app(target=main)
