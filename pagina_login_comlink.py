274d16b7685f195b4c07c38485f68891f7310e01
Alan Medeiros <118941629+AlanMedeirosAG@users.noreply.github.com>
2024-09-27 08:50:51 -0300

    Delete pagina_login_comlink.py

--- pagina_login_comlink.py
+++
@@ -1,7 +0,0 @@
-import flet as ft
-
-# Função para atualizar a tela com o conteúdo da página 'for.py'
-def pagina_login(page: ft.Page):
-    page.clean()  # Limpa a tela atual
-    page.add(ft.Text("Bem-vindo à página de login!"))  # Exemplo de conteúdo
-    page.update()  # Atualiza a página com o novo conteúdo
