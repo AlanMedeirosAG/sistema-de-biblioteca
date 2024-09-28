b00e07564ca7d51c499a017aca3bccb0f038a860
Alan Medeiros <118941629+AlanMedeirosAG@users.noreply.github.com>
2024-09-28 15:44:41 -0300

    Arquivo para iniciar o servidor flask

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from controller.controllers_usuario import usuario_bp
+from controller.controllers_bibliotecario import bibliotecario_bp
+from controller.controllers_administrador import administrador_bp
+from controller.controllers_livro import livro_bp
+from controller.controllers_historico import historico_bp
+from controller.controllers_estoque import estoque_bp
+
+
+app = Flask(__name__)
+
+# Registrar o Blueprint
+app.register_blueprint(usuario_bp)
+app.register_blueprint(bibliotecario_bp)
+app.register_blueprint(administrador_bp)
+app.register_blueprint(livro_bp)
+app.register_blueprint(historico_bp)
+app.register_blueprint(estoque_bp)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
