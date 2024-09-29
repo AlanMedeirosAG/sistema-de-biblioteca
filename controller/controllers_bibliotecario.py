ff2e53a5831b7b61cea17a15445d18d107db7a75
Alan Medeiros <118941629+AlanMedeirosAG@users.noreply.github.com>
2024-09-29 19:39:03 -0300

    Update controllers_bibliotecario.py

--- controller/controllers_bibliotecario.py
+++ controller/controllers_bibliotecario.py
@@ -4,15 +4,29 @@ from model.bibliotecario_model import create_bibliotecario, get_bibliotecarios, 
 bibliotecario_bp = Blueprint('bibliotecario_bp', __name__)
 
 @bibliotecario_bp.route('/bibliotecario', methods=['POST'])
-def create_usuario_route():
+def create_bibliotecario_route():
     data = request.json
     create_bibliotecario(data)
     return jsonify({"message": "bibliotecario inserted successfully"}), 201
 
 @bibliotecario_bp.route('/bibliotecario', methods=['GET'])
-def get_usuario_route():
+def get_bibliotecario_route():
     bibliotecarios = get_bibliotecarios()
     return jsonify(bibliotecarios), 200
+
+@bibliotecario_bp.route('/login_bibliotecario', methods=['POST'])
+def get_bibliotecario_login_route():
+    data = request.get_json()
+    email = data.get('email')
+    senha = data.get('senha')
+    
+    bibliotecario = get_bibliotecario_login(email, senha)
+    
+    if bibliotecario:
+        return jsonify({"message": "Login bem-sucedido!", "bibliotecario": bibliotecario}), 200
+    else:
+        return jsonify({"message": "E-mail ou senha incorretos!"}), 401
+
 
 @bibliotecario_bp.route('/bibliotecario/<int:id>', methods=['PUT'])
 def update_usuario_route(id):
