cea42e2366619ffab7fb736d25e964e4fab3248b
Alan Medeiros <118941629+AlanMedeirosAG@users.noreply.github.com>
2024-10-11 11:32:14 -0300

    Update controllers_bibliotecario.py

--- controller/controllers_bibliotecario.py
+++ controller/controllers_bibliotecario.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from model.bibliotecario_model import create_bibliotecario, get_bibliotecarios, update_bibliotecario, delete_bibliotecario
+from model.bibliotecario_model import create_bibliotecario, get_bibliotecarios, update_bibliotecario,get_bibliotecario_login , delete_bibliotecario
 
 bibliotecario_bp = Blueprint('bibliotecario_bp', __name__)
 
@@ -8,6 +8,24 @@ def create_bibliotecario_route():
     data = request.json
     create_bibliotecario(data)
     return jsonify({"message": "bibliotecario inserted successfully"}), 201
+
+
+# Rota para criar um novo bibliotecario (POST /bibliotecario)
+@bibliotecario_bp.route('/bibliotecario', methods=['POST'])
+def create_bibliotecario_login_route():
+    try:
+        data = request.json
+        # Validação dos dados
+        if 'senha' not in data:
+            return jsonify({"message": "Campo é 'senha' é obrigatório."}), 400
+
+        create_bibliotecario(data)  # Assumindo que sua função já faz as verificações necessárias
+        # Retorno de sucesso
+        return jsonify({"message": "bibliotecario inserido com sucesso!"}), 201
+    
+    except Exception as e:
+        # Tratamento genérico de erros, pode incluir logs aqui
+        return jsonify({"message": f"Erro ao inserir usuário: {str(e)}"}), 500
 
 @bibliotecario_bp.route('/bibliotecario', methods=['GET'])
 def get_bibliotecario_route():
@@ -27,7 +45,6 @@ def get_bibliotecario_login_route():
     else:
         return jsonify({"message": "E-mail ou senha incorretos!"}), 401
 
-
 @bibliotecario_bp.route('/bibliotecario/<int:id>', methods=['PUT'])
 def update_usuario_route(id):
     data = request.json
@@ -35,6 +52,6 @@ def update_usuario_route(id):
     return jsonify({"message": "bibliotecario updated successfully"}), 200
 
 @bibliotecario_bp.route('/bibliotecario/<int:id>', methods=['DELETE'])
-def delete_usuario_route(id):
+def delete_bibliotecario_route(id):
     delete_bibliotecario(id)
     return jsonify({"message": "bibliotecario deleted successfully"}), 200
