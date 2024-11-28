198a62563573637fc4031671654f8029b86ae735
Alan Medeiros <118941629+AlanMedeirosAG@users.noreply.github.com>
2024-11-28 19:56:06 -0300

    Update controllers_historico.py

--- controller/controllers_historico.py
+++ controller/controllers_historico.py
@@ -12,7 +12,7 @@ def create_histotico_route():
     return jsonify({"message": "historico inserted successfully"}), 201
 
 # Rota para obter um historico (GET /historico)
-@historico_bp.route('/historico_bp', methods=['GET'])
+@historico_bp.route('/historico', methods=['GET'])
 def get_histotico_route():
     historico = get_historico()
     return jsonify(historico), 200
@@ -29,3 +29,4 @@ def update_histotico_route(id):
 def delete_histotico_route(id):
     delete_historico(id)
     return jsonify({"message": "historico deleted successfully"}), 200
+    
